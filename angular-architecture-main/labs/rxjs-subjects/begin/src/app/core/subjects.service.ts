2459f07ab22c8dbf6ad887a1a57a1b53c5b031f0
RoxanaD <Roxana.Dina@ortec.com>
2021-11-14 20:04:38 +0200

    Lab2: Add RXJS subject

--- angular-architecture-main/labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
+++ angular-architecture-main/labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SubjectsService {
-  
   //Define Subject and Observable properties here
-
+  subject$: Subject<string>;
+  observable$: Observable<string>;
 
   constructor() {
     this.init();
   }
 
   init() {
-    // Create Subject and Observable Here 
-
+    // Create Subject and Observable Here
+    this.subject$ = new Subject<string>();
+    this.observable$ = this.subject$.asObservable();
 
     // Create interval here
-
+    setInterval(() => {
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}: ${date.getSeconds()}`;
+      this.subject$.next(time);
+    }, 3000);
   }
-
 }
