f9ce2e4bda7e81179e3f2642631a7b8012b4743f
RoxanaD <Roxana.Dina@ortec.com>
2021-11-29 22:53:55 +0200

    Lab 7: Network aware preload strategy

--- angular-architecture-main/labs/routing-guards-and-preload-strategies/begin/src/app/core/network-aware-preload-strategy.ts
+++ angular-architecture-main/labs/routing-guards-and-preload-strategies/begin/src/app/core/network-aware-preload-strategy.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { PreloadingStrategy, Route } from "@angular/router";
-import { Observable, EMPTY } from "rxjs";
+import { Injectable } from '@angular/core';
+import { PreloadingStrategy, Route } from '@angular/router';
+import { Observable, EMPTY } from 'rxjs';
 
 // avoid typing issues for now
 export declare var navigator;
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class NetworkAwarePreloadStrategy implements PreloadingStrategy {
   preload(route: Route, load: () => Observable<any>): Observable<any> {
     return this.hasGoodConnection() ? load() : EMPTY;
@@ -17,8 +17,8 @@ export class NetworkAwarePreloadStrategy implements PreloadingStrategy {
       if (conn.saveData) {
         return false; // save data mode is enabled, so dont preload
       }
-      const avoidTheseConnections = ["slow-2g", "2g" /* , '3g', '4g' */];
-      const effectiveType = conn.effectiveType || "";
+      const avoidTheseConnections = ['slow-2g', '2g', '3g', '4g'];
+      const effectiveType = conn.effectiveType || '';
       if (avoidTheseConnections.includes(effectiveType)) {
         return false;
       }
